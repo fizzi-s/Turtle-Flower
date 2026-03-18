f8aec74a07e4fe95d91369ab0ad4494ed09a819f
fizzi-s <fizziS279@gmail.com>
2026-03-18 09:03:15 +0500

    Enhance flower drawing script with comments
    Updated the flower drawing script to improve speed and added comments for clarity.

--- Creating a flower in python.py
+++ Creating a flower in python.py
@@ -1,23 +1,24 @@
 #Creating a flower in Python 
 import turtle 
-pen = turtle.Turtle()
-pen.speed(2)
+pen = turtle.Turtle()  
+pen.speed(3)
 pen.color("light blue")
-for i in range(6):
+for i in range(6):  #loop draws 6 petals
   pen.begin_fill()
-  pen.circle(100, 60)
-  pen.left(120)
-  pen.circle(100,60)
-  pen.left(120)
-  pen.left(60)
-  pen.end_fill()
- 
-pen.penup()
-pen.goto(0,-20)
-pen.pendown()
+  pen.circle(100, 60)  #1st arc of petal
+  pen.left(120)  
+  pen.circle(100,60)  #2nd arc of petal 
+  pen.left(120)   #positioning for next petal
+  pen.left(60)   #adjusting for symmetry 
+  pen.end_fill()   #fill shape
+
+#Corona of flower 
+pen.penup()   #pen lifted such that no line is drawn
+pen.goto(0,-20)   #pen in the middle of all the petals
+pen.pendown()   #pen down to draw corona
 pen.color("yellow")
 pen.begin_fill()
-pen.circle(20)
+pen.circle(20)    #small circle drawn
 pen.end_fill()
-pen.hideturtle()
+pen.hideturtle()  
 turtle.done()
